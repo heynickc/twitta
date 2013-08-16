650a0c37debc03521993102f7aa438ace79e2030
nickc-ci <nchamberlain@cambridge-intl.com>
2013-08-16 14:22:31 -0400

    Set up to continue on cursors

--- app/app.js
+++ app/app.js
@@ -1,14 +1,26 @@
 var fs = require('fs');
 var Twit = require('twit');
 var _ = require('underscore');
+var util = require('util');
 var creds = require('../data/creds.js');
 
 var T = new Twit(creds);
+var followerIds = [];
 
-T.get('followers/ids', {
-	user_id: 12912
-}, function(err, data) {
-	var buffer = new Buffer(JSON.stringify(data.ids), 'utf-8');
-	var stream = fs.createWriteStream('./test.txt');
-	stream.write(buffer);
-});
+function getFollowers(cursor) {
+	if (cursor === 0) {
+		var wstream = fs.createWriteStream('./test.txt');
+		wstream.write(followerIds);
+	}
+
+	T.get('followers/ids', {
+		user_id: 12912,
+		cursor: cursor
+	}, function(err, data) {
+		if (err) return console.log(util.inspect(err));
+		followerIds = followerIds.concat(data.ids);
+		getFollowers(data.next_cursor);
+	});
+}
+
+getFollowers(-1);
