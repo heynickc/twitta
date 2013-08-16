f8867fe3685b43c9b060807d500155a0b01d4e17
heynickc <nick.chamberlain.jr@gmail.com>
2013-08-15 22:18:37 -0400

    Testing

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
+var fs = require('fs');
 var Twit = require('twit');
+var _ = require('underscore');
 var creds = require('../data/creds.js');
 
 var T = new Twit(creds);
 
-T.get('followers/list', {
-	screen_name: 'heynickc'
+T.get('followers/ids', {
+	user_id: 922
 }, function(err, data) {
-	console.log(data);
+	var buffer = new Buffer(JSON.stringify(data), 'utf-8');
+	var stream = fs.createWriteStream('./test.txt');
+	stream.write(buffer);
 });
