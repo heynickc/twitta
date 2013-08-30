666c44516c8de9721f1533f4de72ebdb333562e5
heynickc <nick.chamberlain.jr@gmail.com>
2013-08-29 21:17:36 -0400

    Plagirized from https://github.com/pilwon/node-twitter-autofollow-bot/blob/18352ff88742f37ef0b09ea169ec303d248374e4/lib/index.js

--- app/app.js
+++ app/app.js
@@ -3,44 +3,100 @@ var Twit = require('twit');
 var _ = require('underscore');
 var util = require('util');
 var async = require('async');
+var log4js = require('log4js');
 
 var creds = require('../data/creds.js');
 
+var logger = log4js.getLogger();
 var T = new Twit(creds);
 var app = {};
 var cursor = -1;
 
 
-function getFollowers(id, callback) {
-	if (cursor === 0) {
-		callback(dataset);
-	} else {
-		T.get('followers/ids', {
-			// user_id: id,
-			screen_name: id,
-			cursor: cursor
-		}, function(err, data) {
-			if (err) {
-				switch (err.statusCode) {
-					case 429:
-						console.log(util.format('Limit reached @ %s, process will resume in 15 min...', cursor));
-						return setTimeout(function() {
-							getFollowers(id, callback);
-						}, 900000);
-					default:
-						return console.log(util.inspect(err));
-				}
+// function getFollowers(id, callback) {
+// 	if (cursor === 0) {
+// 		callback(null, dataset);
+// 	} else {
+// 		T.get('followers/ids', {
+// 			// user_id: id,
+// 			screen_name: id,
+// 			cursor: cursor
+// 		}, function(err, data) {900000
+// 			if (err) {
+// 				switch (err.statusCode) {
+// 					case 429:
+// 						console.log(util.format('Limit reached @ %s, process will resume in 15 min...', cursor));
+// 						return setTimeout(function() {
+// 							getFollowers(id, callback);
+// 						}, );
+// 					default:
+// 						return console.log(util.inspect(err));
+// 				}
+// 			}
+// 			dataset = {
+// 				user: '',
+// 				followers: []
+// 			};
+// 			dataset.user = id;
+// 			dataset.followers = _.union(dataset.followers, data.ids);
+// 			cursor = data.next_cursor;
+// 			getFollowers(id, callback);
+// 		});
+// 	}
+// }
+
+
+
+function _fetchFollowers(nextCursor, id, cb) {
+	T.get('friends/ids', {
+		// user_id: id,
+		screen_name: id,
+		cursor: nextCursor
+	}, function(err, result) {
+		if (err) {
+			switch (err.statusCode) {
+				case 429:
+					logger.debug('Rate limit exceeded (cursor: %s)', nextCursor);
+					return setTimeout(function() {
+						_fetchFollowers(nextCursor, id, cb);
+					}, 900000);
+				default:
+					return cb(err);
 			}
-			dataset = {
-				user: '',
-				followers: []
-			};
-			dataset.user = id;
-			dataset.followers = _.union(dataset.followers, data.ids);
-			cursor = data.next_cursor;
-			getFollowers(id, callback);
-		});
-	}
+		}
+
+		logger.debug('Fetched %d follower IDs (cursor: %s)',
+			result.ids ? result.ids.length : '?',
+			nextCursor);
+
+		dataset = {
+			user: '',
+			friends: []
+		};
+
+		function mergeIds(ids) {
+			if (_.isArray(ids)) {
+				dataset.user = id;
+				dataset.friends = _.union(dataset.friends, ids);
+			}
+		}
+
+		mergeIds(result.ids);
+
+		if (result['next_cursor'] !== 0) {
+			// Fetch next cursor.
+			_fetchFollowers(result['next_cursor'], function(err, ids) {
+				if (err) {
+					return cb(err);
+				}
+				mergeIds(ids);
+				cb(null, dataset);
+			});
+		} else {
+			// Return result.
+			cb(null, dataset);
+		}
+	});
 }
 
 var rstream = fs.createReadStream('../data/sample.csv');
@@ -56,20 +112,17 @@ function write(data) {
 }
 
 rstream.on('data', function(data) {
-	// result = data.split('\r\n');
-	// result = _.filter(result, function(val) {
+	// results = data.split('\r\n');
+	// results = _.filter(result, function(val) {
 	// 	if (!isNaN(val)) {
 	// 		return val;
 	// 	}
 	// });
 
 	results = ['heynickc', 'EmilyMMcKenzie'];
-	async.forEach(results, function(result, callback) {
-			getFollowers(result, function(val) {
-				write(val);
-			});
-		},
-		function(err) {
-			console.log(err);
+	_.each(results, function(item) {
+		_fetchFollowers(-1, item, function(err, data) {
+			write(data);
 		});
+	});
 });
